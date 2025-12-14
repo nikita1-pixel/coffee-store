f9fa3fa0554538af02f075ea1e56afc147ecdcfe
Nikita Choudhary <84392685+nikita1-pixel@users.noreply.github.com>
2025-12-14 13:49:00 +0530

    navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Navbar = () => {  
+    return (
+        <nav className="w-full p-4 bg-white shadow-md">
+            <div className="container mx-auto px-6 py-3">
+                <h1 className="text-2xl font-bold text-gray-800">CoffeeBreeze</h1>
+            </div>
+        </nav>
+    );
+}                                                                                       
+export default Navbar;
